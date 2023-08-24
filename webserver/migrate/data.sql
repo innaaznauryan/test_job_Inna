2fae4b099b18f18586576831730b84336d8eb26b
Ivan Timokhov <forspamorelse@gmail.com>
2023-08-24 07:43:15 -0300

    useless removed

--- webserver/migrate/data.sql
+++
@@ -1,7 +0,0 @@
-SELECT * FROM phonelist;
-
-INSERT IGNORE INTO phonelist (fname, lname, phone, bday) VALUES
-    ("Tom", "Hanks", "+1123456789", "1956-07-09"),
-    ("Will", "Smith", "+1987654321", "1968-09-25"),
-    ("Bruce", "Willis", "+1147258396", "1955-03-19")
-    ;
